41f6ee66537a2ebed56ef74eb781e9936ef73e4e
Tamsyn Naylor <tmnaylor@outlook.com>
2018-03-07 08:03:05 +0200

    Commit from R Studio

--- diag_ratio_T.R
+++ diag_ratio_T.R
@@ -1,6 +1,6 @@
 #Tredoux diagnosticity ratio
 diag_ratio_T <- function(vec1, vec2, x, t){
-    a <- (sum(vec1 == x)+0.5)/(t+1.5)
-    b <-(sum(vec2 == x)+0.5)/(t+2.5)
+    a <- (sum(vec1 == x)+0.5)/(t+0.5)
+    b <-(sum(vec2 == x)+0.5)/(t+0.5)
     c <- a/b
-}
+} 
