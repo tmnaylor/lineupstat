ebc1a308ca6e8b30d11b98be1d086070704f3386
Tamsyn Naylor <tmnaylor@outlook.com>
2018-03-07 12:00:29 +0200

    working function!

---
+++ ln_diag_ratio.R
@@ -0,0 +1,10 @@
+#Computing ln(d) for k lineups
+#Needs df of diagnosticity parameters --> get using helper fx
+ln_diag_ratio <- function(df){
+    d<- (df$n11+0.5/((df$n11+df$n21)+0.5))/
+        (df$n12+0.5/((df$n12+df$n22)+0.5))
+    lnd <- log(d)
+    as.data.frame(lnd)
+    
+    return(lnd)
+}
