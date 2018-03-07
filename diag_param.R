0c9d16b98468981409c46ba9d66a509b8969d9ae
Tamsyn Naylor <tmnaylor@outlook.com>
2018-03-07 10:17:05 +0200

    Commit from Rstudio

---
+++ diag_param.R
@@ -0,0 +1,12 @@
+#Function to compute n11/12 and n21/22 from df of present/absent lineup choices
+diag_param <- function(df, pos){
+    diagdf <- as.data.frame(matrix(ncol = 2, 
+                                    nrow = length(df)))
+    
+    for (i in 1:ncol(df)){
+        diagdf[i,1]= sum(df[i] == pos[i])
+        diagdf[i,2] = sum(df[i] != pos[i])
+        
+    }
+    return(diagdf)
+}
