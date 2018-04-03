a68c176ee73db7fbb56eef5c703c9b7c06b8a265
Tamsyn Naylor <tmnaylor@outlook.com>
2018-04-03 09:56:20 +0200

    Some base functions for bootstrapping

---
+++ allprop_boot_fx.R
@@ -0,0 +1,12 @@
+#Base function for computing proportion for each lineup member
+#Using package boot
+#Takes vector of lineup choices and vector of lineup member positions as inputs
+#Doesn't work yet
+allprop_boot <- function(linevec, d=d, susp_pos){
+    propvec <- as.data.frame(matrix(ncol= 1,
+                                    nrow = length(susp_pos)))
+    for (i in 1:length(susp_pos)){
+        propvec[i,]=lineup_prop_pos_vec(linevec[d], susp_pos[i])
+    }
+    return(propvec)
+}
