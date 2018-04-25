cef5ebc3b772561ca0a15cf727156bbacee85164
Tamsyn Naylor <tmnaylor@outlook.com>
2018-04-25 14:55:50 +0200

    Edits to documentation

--- d_weights.R
+++ d_weights.R
@@ -6,10 +6,8 @@
 #'         we use a weight for each ratio that is equal to the inverse of its
 #'         variance.
 #'
-#'
-#'         linedf dataframe is computed using the diag_param helper function 
-#' 
-#'          diag_param returns a dataframe containing the following:
+#'         linedf dataframe is computed using the diag_param helper function,
+#'          diag_param: returns a dataframe containing the following:
 #'          n11: Number of mock witnesses who identified the suspect in the target
 #'              present condition
 #'          n21: Number of mock witnesses who did not identify the suspect in the
--- rep_index.R
+++ rep_index.R
@@ -3,7 +3,7 @@
 #'Function that repeats a number k times
 #'@details Can be used to create vectors of lineup data
 #'@param index Number to be repeated
-#'@param num Number of times to repeat index number
+#'@param num Number of times to repeat index
 #'@examples
 #'rep_index(1, 10)
 #'[1] 1 1 1 1 1 1 1 1 1 1
